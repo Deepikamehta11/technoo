a01b92fc9406c06768d149d4ed14d22a3223a753
Deepika <Deepika@DESKTOP-1QU0SC0>
2024-07-05 22:09:01 +0530

    hello 1

---
+++ team1/src/team1/mohit.java
@@ -0,0 +1,16 @@
+package team1;
+
+public class mohit {
+	
+	public mohit()
+	{
+		System.out.println("default");
+	}
+	public static void main(String[] args) {
+		
+	}
+	mohit ab=new mohit();
+	
+			
+
+}
